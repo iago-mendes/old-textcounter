a2ba5c7198f04b40ca680eda9feae335a85970d8
Iago <iagobrazmendes@gmail.com>
2020-08-28 11:29:43 -0300

    start menu tabs component

---
+++ src/components/MenuTabs/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function MenuTabs()
+{
+    return (
+        <div id="tabsContainer">
+            <p>Menu Tabs</p>
+        </div>
+    )
+}
+
+export default MenuTabs
