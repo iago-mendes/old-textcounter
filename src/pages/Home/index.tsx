43f1a8e3352f462caf1b033b40c70fd6dee809d1
Iago <iagobrazmendes@gmail.com>
2020-08-30 15:09:11 -0300

    info section created

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { FiCopy, FiMenu } from 'react-icons/fi'
 
 import './styles.css'
 
@@ -6,6 +7,23 @@ function Home()
 {
     return (
         <div id="homeContainer">
+            <div className="info">
+                <span className="showInfo">Words: {500}</span>
+                <span className="showInfo">Characters: {2000}</span>
+                <div className="buttons">
+                    <button title="Copy text" className="copy">
+                        <FiCopy />
+                    </button>
+                    <button title="See options" className="options">
+                        <FiMenu />
+                    </button>
+                </div>
+                <span className="showInfo">Lines: {100}</span>
+                <span className="showInfo">Paragraphs: {25}</span>
+                <div className="lettersContainer">
+                    <button title="See letter density" className="letters">Letter Density</button>
+                </div>
+            </div>
             <textarea name="textarea" className="textarea"></textarea>
         </div>
     )
