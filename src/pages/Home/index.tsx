4b508f4d0c06666e175ae6955231f9da4c66d47e
Iago <iagobrazmendes@gmail.com>
2020-09-03 10:33:23 -0300

    start counting text

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, ChangeEvent } from 'react'
 import { FiCopy, FiMenu } from 'react-icons/fi'
 
 import './styles.css'
@@ -9,12 +9,27 @@ function Home()
     const [isOptionsOpen, setIsOptionsOpen] = useState(false)
     const [showInfo, setShowInfo] = useState<ShowInfo>(defaultShowInfo)
 
+    const [text, setText] = useState<string>('')
+    const [info, setInfo] = useState(
+    {
+        words: 0,
+        characters: 0,
+        lines: 0,
+        paragraphs: 0
+    })
+
+    function handleTextChange(e: ChangeEvent<HTMLTextAreaElement>)
+    {
+        const {value} = e.target
+        setText(value)
+    }
+
     return (
         <div id="homeContainer">
             <Options isOpen={isOptionsOpen} setIsOpen={setIsOptionsOpen} showInfo={showInfo} setShowInfo={setShowInfo} />
             <div className="info">
                 {showInfo.words && <span className="showInfo">Words: {500}</span>}
-                {showInfo.characters && <span className="showInfo">Characters: {2000}</span>}
+                {showInfo.characters && <span className="showInfo">Characters: {text.length}</span>}
                 <div className="buttons">
                     <button title="Copy text" className="copy">
                         <FiCopy />
@@ -33,7 +48,7 @@ function Home()
                     <button title="See letter density" className="letters">Letter Density</button>
                 </div>}
             </div>
-            <textarea name="textarea" className="textarea"></textarea>
+            <textarea onChange={handleTextChange} name="textarea" className="textarea"></textarea>
         </div>
     )
 }
