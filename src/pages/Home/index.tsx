63e7ebcb33fa3b8b7055d6da7ef1bbd69652a77a
Iago <iagobrazmendes@gmail.com>
2020-09-05 12:34:39 -0300

    count info

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, ChangeEvent } from 'react'
+import React, { useState, ChangeEvent, useEffect } from 'react'
 import { FiCopy, FiMenu } from 'react-icons/fi'
 
 import './styles.css'
@@ -14,9 +14,22 @@ function Home()
     {
         words: 0,
         characters: 0,
-        lines: 0,
         paragraphs: 0
     })
+
+    useEffect(() =>
+    {
+        const getWords = text.length === 0 ? [] : text.replace('\n', ' ').split(' ').filter(item => {return item != ''})
+        const getParagraphs = text.split('\n').filter(item => {return item != ''})
+        const tmpInfo =
+        {
+            words: getWords.length,
+            characters: text.length,
+            paragraphs: getParagraphs.length
+        }
+
+        setInfo(tmpInfo)
+    }, [text])
 
     function handleTextChange(e: ChangeEvent<HTMLTextAreaElement>)
     {
@@ -29,9 +42,9 @@ function Home()
             <Options isOpen={isOptionsOpen} setIsOpen={setIsOptionsOpen} showInfo={showInfo} setShowInfo={setShowInfo} />
             <div className="infoButtonsContainer">
                 <div className="info">
-                    {showInfo.words && <span className="showInfo">Words: {500}</span>}
-                    {showInfo.characters && <span className="showInfo">Characters: {text.length}</span>}
-                    {showInfo.paragraphs && <span className="showInfo">Paragraphs: {25}</span>}
+                    {showInfo.words && <span className="showInfo">Words: {info.words}</span>}
+                    {showInfo.characters && <span className="showInfo">Characters: {info.characters}</span>}
+                    {showInfo.paragraphs && <span className="showInfo">Paragraphs: {info.paragraphs}</span>}
                     {showInfo.letters && <div className="lettersContainer">
                         <button title="See letter density" className="letters">Letter Density</button>
                     </div>}
