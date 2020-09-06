d30d25b40857b4b1b3fbc6941fa1f4290a6957b3
Iago <iagobrazmendes@gmail.com>
2020-09-06 16:52:45 -0300

    start working with theme change

--- src/components/MenuTabs/index.tsx
+++ src/components/MenuTabs/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useState, ChangeEvent } from 'react'
 import {Link} from 'react-router-dom'
-import Switch from 'react-input-switch'
+import Switch from 'react-switch'
 
 import logoDark from '../../assets/logo-dark.svg'
 
@@ -8,6 +8,35 @@ import './styles.css'
 
 function MenuTabs()
 {
+    const [isDark, setIsDark] = useState(false)
+
+    async function handleChangeTheme()
+    {
+        await setIsDark(!isDark)
+        
+        if (!isDark) // dark theme
+        {
+            localStorage.setItem('@text-counter/theme', 'dark')
+            document.documentElement.style.setProperty('--primary', '#282a36')
+            document.documentElement.style.setProperty('--secondary', '#44475a')
+            document.documentElement.style.setProperty('--secondary-strong', '#6272a4')
+            document.documentElement.style.setProperty('--text', '#f8f8f2')
+            document.documentElement.style.setProperty('--textarea', '#bd93f9')
+            document.documentElement.style.setProperty('--textarea-focus', '#ff79c6')
+        }
+
+        if (isDark) // light theme
+        {
+            localStorage.setItem('@text-counter/theme', 'light')
+            document.documentElement.style.setProperty('--primary', '#f5f5f5')
+            document.documentElement.style.setProperty('--secondary', '#44475a')
+            document.documentElement.style.setProperty('--secondary-strong', '#6272a4')
+            document.documentElement.style.setProperty('--text', '#44475a')
+            document.documentElement.style.setProperty('--textarea', '#bd93f9')
+            document.documentElement.style.setProperty('--textarea-focus', '#ff79c6')
+        }
+    }
+
     return (
         <div id="tabsContainer">
             <Link to="/" className="logo">
@@ -15,7 +44,10 @@ function MenuTabs()
                 <span>Text Counter</span>
             </Link>
             <div className="buttons">
-                <Switch onChange={() => {}} />
+                <Switch
+                    checked={isDark}
+                    onChange={handleChangeTheme}
+                />
                 <Link to="/about" className="about">
                     <span>About</span>
                 </Link>
