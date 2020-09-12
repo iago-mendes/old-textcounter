6bdb02ba03ced7e339f4bd0a663fd98baa7704f8
Iago <iagobrazmendes@gmail.com>
2020-09-12 17:00:58 -0300

    style about page

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 
 import logoDark from '../../assets/logo-dark.svg'
+import creator from '../../assets/creator.jpg'
+
+import './styles.css'
 
 function About()
 {
@@ -13,14 +16,15 @@ function About()
             <div className="card">
                 <h2>Who am I?</h2>
                 <ul>
-                    <li>
-                        <img src={logoDark} alt="robot"/>
-                        <p>My name is Text Counter and I can help you count your text (who would’ve guessed, right?).<br/>You can use me as a simple counter of words and characters or you can enable some of my show options and features. It’s your choice!</p>
-                    </li>
-                    <li>
-                        <p>I was developed by Iago Mendes, who is a college student passionate about coding. You can see more info about him in his GitHub and LikedIn accounts.<br/>He also made me as an open source application, so you can see my repository here.</p>
-                        <img src={'../../assets/creator.jpg'} alt="creator"/>
-                    </li>
+                    <img src={logoDark} alt="robot"/>
+                    <p>My name is Text Counter and I can help you count your text (who would’ve guessed, right?).<br/><br/>You can use me as a simple counter of words and characters or you can enable some of my show options and features. It’s your choice!</p>
+                </ul>
+            </div>
+            <div className="card">
+                <h2>Who is my creator?</h2>
+                <ul>
+                    <p>I was developed by Iago Mendes, who is a college student passionate about coding. You can see more info about him in his GitHub and LikedIn accounts.<br/><br/>He also made me as an open source application, so you can see my repository here.</p>
+                    <img src={creator} alt="creator" style={{borderRadius: 175}} />
                 </ul>
             </div>
         </div>
