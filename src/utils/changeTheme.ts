3a1d001089464d8958d89591665af28ae5279b15
Iago <iagobrazmendes@gmail.com>
2020-09-29 11:25:25 -0300

    change favicon dinamically

--- src/utils/changeTheme.ts
+++ src/utils/changeTheme.ts
@@ -1,7 +1,9 @@
 export default function changeTheme(theme: string)
 {
+    const favicon = (<HTMLLinkElement>document.getElementById('favicon'))
     if (theme === 'dark')
     {
+        if (favicon !== null) favicon.href = "dark-logo.png"
         document.documentElement.style.setProperty('--primary', '#282a36')
         document.documentElement.style.setProperty('--secondary', '#44475a')
         document.documentElement.style.setProperty('--secondary-strong', '#6272a4')
@@ -11,6 +13,7 @@ export default function changeTheme(theme: string)
     }
     else if (theme === 'light')
     {
+        if (favicon !== null) favicon.href = "light-logo.png"
         document.documentElement.style.setProperty('--primary', '#f5f5f5')
         document.documentElement.style.setProperty('--secondary', '#ffe4ad')
         document.documentElement.style.setProperty('--secondary-strong', '#ffad05')
